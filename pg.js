0b4a1c374c591d776bf5465387fc06de705a453b
Gideon Maina <gmainapro@yahoo.com>
2023-09-18 20:48:30 +0300

    Initial setup

---
+++ pg.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const { Client } = require("pg");
+const client = new Client(process.env.POSTGRES_URL);
+
+async function testPG() {
+  //   const client = new Client();
+  await client.connect();
+  const res = await client.query("SELECT $1::text as message", [
+    "Postgres is available",
+  ]);
+
+  await client.end();
+  return res.rows[0].message;
+}
+
+module.exports = { testPG };
