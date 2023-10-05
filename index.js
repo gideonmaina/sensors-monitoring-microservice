7a027bcd9512db3e3fe0e011e7d62f3d8895c2d8
Giddie <gmainapro@yahoo.com>
2023-10-05 14:41:07 +0000

    Add endpoint to send firmware  update files

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require('path')
 const { testPG, getInactiveNodes, getActiveNodes } = require("./pg");
+const fs = require('fs')
+const md5 = require("md5-file");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
+}); 
 app.get("/pg-test", async (req, res) => {
   const pgtest = await testPG();
   res.send(pgtest);
@@ -17,6 +21,32 @@ app.get("/active-nodes", async (req, res) => {
   const activeNodes = await getActiveNodes();
   res.send(activeNodes);
 });
+
+
+
+app.get('/firmware-update', function(req, res, next) {
+ //console.log(req.headers);
+let filePath = path.join(__dirname, '/firmwares/ota-test.bin');
+
+ //console.log(filePath)
+ let options = {
+  // root:filePath,
+  headers: {
+   "x-MD5":  md5.sync(filePath)
+  }
+ }
+ res.sendFile(filePath, options, function (err) {
+  if (err) {
+   next(err)
+  } else {
+   console.log('Sent:', filePath)
+  }
+ });
+});
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
