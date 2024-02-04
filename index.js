89a047c47862b1c1ce895ffe17f2efbaa588d7c2
gideon maina <gmainapro@yahoo.com>
2024-02-04 11:24:24 +0300

    Add firmware loader routes and resources

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/firmware-update", function (req, res, next) {
       "x-MD5": md5.sync(filePath),
     },
   };
-  res.sendFile(filexPath, options, function (err) {
+  res.sendFile(filePath, options, function (err) {
     if (err) {
       next(err);
     } else {
@@ -43,12 +43,7 @@ app.get("/firmware-update", function (req, res, next) {
 });
 
 app.get("/firmware-update/checksum", (req, res) => {
-  let filePath = path.join(__dirname, "/firmwares/checksum.txt");
-  // if (!fs.existsSync(filePath)) {
-  //   console.log("Checksum file does not exist");
-  //   res.send("null");
-  // }
-
+  let filePath = path.join(__dirname, "firmwares/checksum.txt");
   fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
       console.log("Checksum file does not exist");
@@ -64,6 +59,54 @@ app.get("/firmware-update/checksum", (req, res) => {
       console.error(err);
       res.send(err);
     }
+  });
+});
+
+//Firmware loader routes
+app.get("/firmware/loader/checksum", (req, res) => {
+  let filePath = path.join(__dirname, "firmwares/loader/loader-002.bin.md5");
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log("Loader checksum file does not exist");
+      res.send("Loader checksum file does not exists");
+      return;
+    }
+
+    try {
+      const data = fs.readFileSync(filePath, "utf8");
+      console.log(data);
+      res.send(data);
+    } catch (err) {
+      console.error(err);
+      res.send(err);
+    }
+  });
+});
+
+app.get("/firmware/loader", (req, res) => {
+  let filePath = path.join(__dirname, "firmwares/loader/loader-002.bin");
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log("Loader bin file does not exist");
+
+      res.send("Loader_not_found");
+      return;
+    }
+
+    let options = {
+      // root:filePath,
+      headers: {
+        "x-MD5": md5.sync(filePath),
+      },
+    };
+    res.sendFile(filePath, options, function (err) {
+      if (err) {
+        next(err);
+      } else {
+        console.log("Sent:", filePath);
+      }
+    });
   });
 });
 
